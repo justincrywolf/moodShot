e4c31f2b3d630367dc11a584f74344a3bb601087
justincrywolf <justincrywolf@gmail.com>
2018-03-30 14:30:39 -0400

    updating js file

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 // click handler for submitting image
+// $( "#submit" ).on( "click", function(){
+//     var faceURL = $(this).val
+
+
+    // moving image file to some form that can pass into query
 
 
     // global variables for face++
 
-    var faceImage = $(this).val;
-    var faceQuery = "https://api-us.faceplusplus.com/facepp/v3/detect?api_key=s0vdP0fNEcYFymfT0sw0pt-pGCOp37-y&api_secret=7Ri6sHu3XL0WdXmylro7Rvz_v8JJKV_M&image_file=" + faceImage + "&return_attributes=emotion";
+    //////////////////////////// var faceImage = user uploaded image
+    var faceImage = "https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/13625394_10154011762247869_1447612658950188866_n.jpg?_nc_cat=0%26oh=9af591b71b9810b065670cc7da47f56a%26oe=5B6BA40E";
 
-        // face++ ajax request
-        
-        $.ajax({
+
+    var faceQuery = "https://cors-anywhere.herokuapp.com/https://api-us.faceplusplus.com/facepp/v3/detect?api_key=s0vdP0fNEcYFymfT0sw0pt-pGCOp37-y&api_secret=7Ri6sHu3XL0WdXmylro7Rvz_v8JJKV_M&image_url=" + faceImage + "&return_attributes=emotion";
+
+    // face++ ajax request
+    var emotions;
+
+    $.ajax({
         url: faceQuery,
-        method: "GET"
+        method: "POST"
         }).then(function(response) {
-        console.log(response);
+        var emotions = response.faces[0].attributes.emotion;
+        console.log(emotions);
+        function sortProperties( obj ){
+         // convert object into array
+	    var sortable=[];
+	    for(var key in obj)
+		    if(obj.hasOwnProperty(key))
+			    sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+	
+        // sort items by value
+        sortable.sort(function(a, b)
+        {
+        return a[1]-b[1]; // compare numbers
         });
+        return sortable; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+        }
+
+        var sortedEmotions = sortProperties(emotions);
+        console.log(sortedEmotions.reverse());
+        });
+
 
     // global variables for cocktailDB
 
 
         // cocktailDB ajax request
 
+
+
+// });
